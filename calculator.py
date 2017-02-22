dfae75d56ae2c7b978a3bf4b34b08be4c2d5a838
Dmitry Petrov <komly@yandex.ru>
2017-02-23 01:18:11 +0300

    Add subtraction feature

--- calculator.py
+++ calculator.py
@@ -2,3 +2,6 @@ class Calc:
     def add(self, a, b):
         return a + b
 
+    def sub(self, a, b):
+        return a - b
+
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -8,3 +8,7 @@ def test_calc_can_add():
     calc = Calc()
     assert calc.add(1, 2) == 3
     assert calc.add(2, 2) == 4
+
+def test_calc_can_sub():
+    calc = Calc()
+    assert calc.sub(4, 3) == 1
