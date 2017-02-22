34ede163c0f82e4e49201567d88affcc41b4f029
Dmitry Petrov <komly@yandex.ru>
2017-02-23 02:00:54 +0300

    Add verbose mode support

--- calculator.py
+++ calculator.py
@@ -1,11 +1,37 @@
+class SoBigException(Exception):
+    pass
+
+
 class Calc:
     MODE_NORMAL = 0
     MODE_VERBOSE = 1
+    WORD_NUM_MAP = {
+            'ZERO': 0,
+            'ONE': 1,
+            'TWO': 2,
+            'THREE': 3,
+            'FOUR': 4,
+            'FIVE': 5,
+            'SIX': 6,
+            'SEVEN': 7,
+            'EIGHT': 8,
+            'NINE': 9,
+            'TEN': 10,
+    }
     def __init__(self, mode=MODE_NORMAL):
         self.mode = mode
 
     def add(self, a, b):
-        return a + b
+        if self.mode == Calc.MODE_NORMAL:
+            return a + b
+        elif self.mode == Calc.MODE_VERBOSE:
+            a = Calc.WORD_NUM_MAP[a]
+            b = Calc.WORD_NUM_MAP[b]
+            rev_map = dict(map(lambda p: (p[1], p[0]), Calc.WORD_NUM_MAP.items()))
+            res = a + b
+            if res > 10:
+                raise SoBigException
+            return rev_map[res]
 
     def sub(self, a, b):
         return a - b
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,4 +1,5 @@
-from calculator import Calc
+import pytest
+from calculator import Calc, SoBigException
 
 def test_calc():
     calc = Calc()
@@ -13,9 +14,21 @@ def test_calc_can_sub():
     calc = Calc()
     assert calc.sub(4, 3) == 1
 
-def test_calc_verbose_mode():
+def test_calc_mode_argument():
     calc = Calc()
     assert calc.mode == calc.MODE_NORMAL
     
     calc = Calc(mode=Calc.MODE_VERBOSE)
     assert calc.mode == calc.MODE_VERBOSE
+
+def test_calc_verbose_mode_add():
+    calc = Calc(mode=Calc.MODE_VERBOSE)
+    assert calc.add('ONE', 'ONE') == 'TWO'
+    assert calc.add('ONE', 'TWO') == 'THREE'
+    assert calc.add('ONE', 'THREE') == 'FOUR'
+    assert calc.add('FIVE', 'FIVE') == 'TEN'
+
+def test_calc_raises_exception_if_num_so_big():
+    calc = Calc(mode=Calc.MODE_VERBOSE)
+    with pytest.raises(SoBigException):
+        calc.add('FIVE', 'SIX')
